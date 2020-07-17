38bac04ecde6aac20d44a09a6a5030224449b219
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-07-17 14:15:04 +0530

    registering user and profile update

--- server.js
+++ server.js
@@ -63,7 +63,6 @@ app.post('/register', (req, res) => {
         id:'125',
         name: name,
         email: email,
-        password: password,
         entries:0,
         joined: new Date()
     })
@@ -82,7 +81,7 @@ app.get('/profile/:id', (req, res)=> {
         res.status(400).json('not found')
     }
 })
-app.post('/image', (req, res) => {
+app.put('/image', (req, res) => {
     const { id } = req.body;
     let found = false;
     database.users.forEach(user => {
